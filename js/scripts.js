24ed9459b875045327e854b481e735cb9be2ff61
Maksym <maksymilian.makuch7@gmail.com>
2017-10-02 19:27:26 +0200

    Działa jak trzeba

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,4 @@ var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 var dinosaur = 'triceratops';
 var dinosaurUC = dinosaur.toUpperCase();
 var text2 = text.replace('Velociraptor', dinosaurUC);
-console.log(text2.length/2);
-console.log(text2.substr(0, 72));
+console.log(text2.substr(0,  text2.length/2 ));
